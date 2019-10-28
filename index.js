1f9e40d00dcc11a8e8dc462cdab034cb726fe4e5
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-10-28 14:49:46 -0700

    seeing users in browser, from api

--- index.js
+++ index.js
@@ -9,22 +9,35 @@ server.get('/', (req, res) => {
     res.send('Server is talking!');
 });
 
+server.get('/api/users', (req, res) => {
+    const users = [
+        {
+        id: 1,
+        name: "Jane Doe", // String, required
+        bio: "Not Tarzan's Wife, another Jane",  // String
+        // created_at: Mon Oct 28 12:50:16 GMT-0700 (PDT), // Date, defaults to current date
+        // updated_at: Mon Oct 28 12:50:16 GMT-0700 (PDT) // Date, defaults to current date
+        }
+    ];
+
+    res.status(200).send(users);
+})
+
+server.post('/api/users', (req, res) => {
+    const usersInfo = req.body;
+    
+    console.log('users info: ', usersInfo);
+    db.add(usersInfo)
+    .then(users => {
+        res.status(201).json(users);
+    })
+    .catch(err => {
+        console.log('error', err);
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+        
+    })
+});
+
 server.listen(8000, () => {
     console.log('API is running on port 8000');
 });
-
-// server.post('/api/users', (req, res) => {
-//     const usersInfo = req.body;
-
-//     console.log('users info: ', usersInfo);
-//     db.add(usersInfo)
-//         .then(users => {
-//             res.status(201).json(users);
-//         })
-//         .catch(err => {
-//             console.log('error', err);
-//             res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
-
-//         })
-// });
-
